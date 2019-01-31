7ab26d3fafc87bc26b6153cc3c87bd901dc3d019
Christopher Gotuaco <christophergotuaco@christophersmbp.dhcp.nd.edu>
2019-01-31 18:56:29 -0500

    Integrated parse and almost fixed layout

--- frontend/app.js
+++ frontend/app.js
@@ -1,8 +1,30 @@
-var mealplaND = angular.module("mealplaND", []);
+var mealplaND = angular.module('mealplaND', ['ngParse']);
 
-mealplaND.controller('mainController', ['$scope', '$http', function ($scope, $http) {
+angular.module('mealplaND')
+  .config(['ParseProvider', function(ParseProvider) {
+      var MY_PARSE_APP_ID = 'Y5SVSIlce78gzRrm2D59XGVC64r8ILT0FNU2hj38';
+      var MY_PARSE_JS_KEY = 'TP08thawFDAbDa6pTiveKJRYzZADFp27OQK3qCCL';
+      
     
       
+    ParseProvider.initialize(MY_PARSE_APP_ID, MY_PARSE_JS_KEY);
+      
+      ParseProvider.serverURL = 'https://parseapi.back4app.com'; // This is your Server URL
+      
+  }]);
+
+mealplaND.controller('mainController', ['$scope', '$http', 'MealModel', function ($scope, $http, MealModel) {
+    
+MealModel.getById('aKHBUo6wGi').then(function(result){
+    console.log('meal result: ', result)
+    
+})
+        
+MealModel.getAllAgencies().then(function(result){
+    console.log('all agencies result: ', result)
+})
+    
+    
 $http({
   method: 'GET',
   url: 'http://localhost:3000/all'
@@ -18,3 +40,77 @@ $http({
   });
     
 }])
+
+/**
+ * @ngdoc service
+ * @name common.service:AgencyModel
+ *
+ * @description Model and helper methods for Agency parse object.
+ */
+
+
+var MealModel = function(Parse) {
+        this.Parse = Parse;
+        this.data = {};
+        this.collection = [];
+        this.name = 'Meal';
+        this.fields = [
+            'name',
+            'protein',
+            'carbs',
+            'calories',
+            'tags',
+            'fat',
+            'allergies'
+        ];
+    
+        this.New = New;
+        this.getById = getById;
+        this.getAllAgencies = getAllAgencies;
+
+    function New(obj) {
+        if (angular.isUndefined(obj)) {
+            const parseObject = new this.Parse.Object(this.name)
+            this.Parse.defineAttributes(parseObject, this.fields);
+            return parseObject;
+        } else {
+            this.Parse.defineAttributes(obj, this.fields);
+            return obj;
+        }
+    }
+    function getById(id) {
+        return new this.Parse.Query(this.New()).get(id)
+            .then(result => {
+                this.Parse.defineAttributes(result, this.fields);
+                this.data = result;
+                return Promise.resolve(result);
+            }).catch(error => Promise.reject(error));
+    }
+//    function getByName(name) {
+//        console.log('name', name)
+//        return new this.Parse.Query(this.New())
+//            .equalTo('name', name)
+//            .first()
+//            .then(result => {
+//                this.Parse.defineAttributes(result, this.fields);
+//                this.data = result;
+//                console.log('result', result)
+//                return result
+//            })
+//    }
+    function getAllAgencies() {
+        return new this.Parse.Query(this.New()).find(agencies => {
+            agencies.forEach(agency =>
+                this.Parse.defineAttributes(agency, this.fields)
+            );
+            this.collection = agencies;
+            return Promise.resolve(agencies);
+        }).catch(error => Promise.reject(error));
+    }
+}
+
+MealModel.$inject = ['Parse']
+
+angular
+    .module('mealplaND')
+    .service('MealModel', MealModel);
